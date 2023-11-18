edf26cd21eb43e54b653da2db10a4bdf5cd5d7f5
Abhinay Omkar <abhiomkar@gmail.com>
2023-11-18 22:01:41 +0530

    rss feed changes, posts to blog rename

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,9 +3,10 @@ import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 export async function GET(context) {
   return rss({
     title: "Abhinay Omkar",
-    description: "Abhinay Omkar's blog",
+    description: "Abhinay Omkar's Blog",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./posts/*.md")),
+    items: await pagesGlobToRssItems(import.meta.glob("./blog/*.md")),
     customData: `<language>en-us</language>`,
+    stylesheet: "public/rss/pretty-feed-v3.xsl",
   });
 }
