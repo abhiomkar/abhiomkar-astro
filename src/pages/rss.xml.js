2eaa472ce6ad7672b76cb9305a7cf78d5fda64d0
Abhinay Omkar <abhiomkar@gmail.com>
2023-11-19 15:26:08 +0530

    Fix import path dir for rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,7 +5,7 @@ export async function GET(context) {
     title: "Abhinay Omkar",
     description: "Abhinay Omkar's Blog",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./blog/*.md")),
+    items: await pagesGlobToRssItems(import.meta.glob("./pages/blog/*.md")),
     customData: `<language>en-us</language>`,
     stylesheet: "public/rss/pretty-feed-v3.xsl",
   });
