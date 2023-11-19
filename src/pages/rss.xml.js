b6c990e7e9ac84545ed2f047eadb409131ac1cad
Abhinay Omkar <abhiomkar@gmail.com>
2023-11-19 15:30:26 +0530

    fix rss 1

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -7,6 +7,6 @@ export async function GET(context) {
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob("./blog/*.md")),
     customData: `<language>en-us</language>`,
-    stylesheet: "public/rss/pretty-feed-v3.xsl",
+    stylesheet: "rss/pretty-feed-v3.xsl",
   });
 }
